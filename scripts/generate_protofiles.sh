0c63904243a2c6d00a6c1fdb076a57555f4d5866
Michael Thelin <thelinmichael@spotify.com>
2014-04-19 01:07:44 +0200

    Change proto script to accomodate the path change (doh)

--- scripts/generate_protofiles.sh
+++ scripts/generate_protofiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SRC_DIR=src/main/proto
-DEST_DIR=src/main/java
+SRC_DIR=../src/main/proto
+DEST_DIR=../src/main/java
 FILE=*.proto
 
 protoc -I=$SRC_DIR --java_out=$DEST_DIR $SRC_DIR/$FILE
