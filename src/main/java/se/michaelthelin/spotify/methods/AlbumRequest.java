b1ec6ef36fd28ce673e09388f66ee71031e42e9d
Michael Thelin <thelinmichael@spotify.com>
2014-04-22 19:02:49 +0200

    Make album request asynchronous

--- src/main/java/se/michaelthelin/spotify/methods/AlbumRequest.java
+++ src/main/java/se/michaelthelin/spotify/methods/AlbumRequest.java
@@ -1,9 +1,17 @@
 package se.michaelthelin.spotify.methods;
 
 import com.google.common.base.Joiner;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
 import se.michaelthelin.spotify.JsonUtil;
+import se.michaelthelin.spotify.SpotifyProtos;
 import se.michaelthelin.spotify.SpotifyProtos.Album;
 import se.michaelthelin.spotify.SpotifyProtos.AlbumType;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
 
 public class AlbumRequest extends AbstractRequest {
 
@@ -11,8 +19,14 @@ public class AlbumRequest extends AbstractRequest {
     super(builder);
   }
 
-  public Album getAlbum() {
-    return JsonUtil.createAlbum(this.getJson());
+  public ListenableFuture<Album> getAlbum() {
+    ListeningExecutorService service = MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(10));
+    ListenableFuture<Album> albumFuture = service.submit(new Callable<Album>() {
+      public Album call() {
+        return JsonUtil.createAlbum(getJson());
+      }
+    });
+    return albumFuture;
   }
 
   public static Builder builder() {
--- src/test/java/se/michaelthelin/spotify/methods/AlbumRequestTest.java
+++ src/test/java/se/michaelthelin/spotify/methods/AlbumRequestTest.java
@@ -1,5 +1,10 @@
 package se.michaelthelin.spotify.methods;
 
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.MoreExecutors;
+import junit.framework.TestCase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
@@ -7,6 +12,9 @@ import se.michaelthelin.spotify.Api;
 import se.michaelthelin.spotify.JsonUtilTest;
 import se.michaelthelin.spotify.SpotifyProtos.Album;
 
+import java.util.concurrent.ExecutionException;
+
+import static junit.framework.Assert.fail;
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertNotNull;
 import static org.mockito.Mockito.when;
@@ -17,16 +25,23 @@ public class AlbumRequestTest {
 
   @Test
   public void shouldGetAlbumResult() throws Exception {
-    Api api = Api.DEFAULT_API;
-    AlbumRequest request = api.album().id("7e0ij2fpWaxOEHv5fUYZjd").build();
+    final Api api = Api.DEFAULT_API;
+    final AlbumRequest request = api.album().id("7e0ij2fpWaxOEHv5fUYZjd").build();
 
     // Mock response
-    String albumResponseFixture = JsonUtilTest.readTestData("album.json");
+    final String albumResponseFixture = JsonUtilTest.readTestData("album.json");
     AlbumRequest spy = spy(request);
     when(spy.getJson()).thenReturn(albumResponseFixture);
 
-    Album album = spy.getAlbum();
-    assertNotNull(album);
-    assertEquals("0sNOF9WDwhWunNAHPD3Baj", album.getId());
+    ListenableFuture<Album> albumFuture = spy.getAlbum();
+    Futures.addCallback(albumFuture, new FutureCallback<Album>() {
+      public void onSuccess(Album album) {
+        assertNotNull(album);
+        assertEquals("0sNOF9WDwhWunNAHPD3Baj", album.getId());
+      }
+      public void onFailure(Throwable thrown) {
+        fail("Failed to resolve future");
+      }
+    });
   }
 }
