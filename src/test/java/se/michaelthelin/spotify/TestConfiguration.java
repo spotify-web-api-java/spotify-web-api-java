b5555a412a01ac16b4c7f441e91487d51651308c
Michael Thelin <thelinmichael@spotify.com>
2014-05-16 23:46:12 +0200

    Optional to run multiget albums against live instead of mock

--- src/test/java/se/michaelthelin/spotify/TestConfiguration.java
+++ src/test/java/se/michaelthelin/spotify/TestConfiguration.java
@@ -2,6 +2,6 @@ package se.michaelthelin.spotify;
 
 public class TestConfiguration {
 
-  public static boolean USE_MOCK_RESPONSES = false;
+  public static boolean USE_MOCK_RESPONSES = true;
 
 }
--- src/test/java/se/michaelthelin/spotify/methods/AlbumsRequestTest.java
+++ src/test/java/se/michaelthelin/spotify/methods/AlbumsRequestTest.java
@@ -20,6 +20,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNull;
 import static junit.framework.TestCase.assertTrue;
 import static junit.framework.TestCase.fail;
 
@@ -80,8 +81,11 @@ public class AlbumsRequestTest {
   public void shouldGetAlbumResultForIds_sync() throws Exception {
     final Api api = Api.DEFAULT_API;
 
-    final HttpManager mockedHttpManager = TestUtil.MockedHttpManager.returningJson("albums.json");
-    final AlbumsRequest request = api.getAlbums("41MnTivkwTO3UUJ8DrqEJJ").httpManager(mockedHttpManager).build();
+    final AlbumsRequest.Builder requestBuilder = api.getAlbums("41MnTivkwTO3UUJ8DrqEJJ");
+    if (TestConfiguration.USE_MOCK_RESPONSES) {
+      requestBuilder.httpManager(TestUtil.MockedHttpManager.returningJson("albums.json"));
+    }
+    final AlbumsRequest request = requestBuilder.build();
 
     List<Album> albums = request.get();
 
@@ -89,6 +93,22 @@ public class AlbumsRequestTest {
 
     Album firstAlbum = albums.get(0);
     assertEquals("41MnTivkwTO3UUJ8DrqEJJ", firstAlbum.getId());
+    assertEquals(AlbumType.ALBUM, firstAlbum.getAlbumType());
+    assertTrue(firstAlbum.getReleaseDate().getDate().equals(8));
+    assertTrue(firstAlbum.getReleaseDate().getMonth().equals(11));
+    assertEquals(2013, firstAlbum.getReleaseDate().getYear());
+
+    List<SimpleArtist> artists = firstAlbum.getArtists();
+    SimpleArtist firstArtist = artists.get(0);
+    assertEquals("https://api.spotify.com/v1/artists/53A0W3U0s8diEn9RhXQhVz", firstArtist.getHref());
+    assertEquals("53A0W3U0s8diEn9RhXQhVz", firstArtist.getId());
+
+    Page<SimpleTrack> tracksPage = firstAlbum.getTracks();
+    assertEquals("https://api.spotify.com/v1/albums/41MnTivkwTO3UUJ8DrqEJJ/tracks?offset=0&limit=50", tracksPage.getHref());
+    assertEquals(0, tracksPage.getOffset());
+    assertEquals(50, tracksPage.getLimit());
+    assertEquals(38, tracksPage.getTotal());
+    assertEquals("4r9PmSmbAOOWqaGWLf6M9Q", tracksPage.getItems().get(0).getId());
   }
 
   @Test
@@ -96,7 +116,11 @@ public class AlbumsRequestTest {
     final Api api = Api.DEFAULT_API;
 
     final HttpManager mockedHttpManager = TestUtil.MockedHttpManager.returningJson("error_bad-field.json");
-    final AlbumsRequest request = api.getAlbums("41MnTivkwTO3UUJ8DrqEJJ").httpManager(mockedHttpManager).build();
+    final AlbumsRequest.Builder requestBuilder = api.getAlbums("41MnTivkwTO3UUJ8DrqEJJ,你好");
+    if (TestConfiguration.USE_MOCK_RESPONSES) {
+      requestBuilder.httpManager(mockedHttpManager).build();
+    }
+    final AlbumsRequest request = requestBuilder.build();
 
     final CountDownLatch asyncCompleted = new CountDownLatch(1);
 
@@ -123,8 +147,11 @@ public class AlbumsRequestTest {
   public void shouldFailForNotFound_async() throws Exception {
     final Api api = Api.DEFAULT_API;
 
-    final HttpManager mockedHttpManager = TestUtil.MockedHttpManager.returningJson("error_id-not-found.json");
-    final AlbumsRequest request = api.getAlbums("41MnTivkwTO3UUJ8DrqEJJ").httpManager(mockedHttpManager).build();
+    final AlbumsRequest.Builder requestBuilder = api.getAlbums("idontexist");
+    if (TestConfiguration.USE_MOCK_RESPONSES) {
+      requestBuilder.httpManager(TestUtil.MockedHttpManager.returningJson("albums-none-found.json"));
+    }
+    final AlbumsRequest request = requestBuilder.build();
 
     final CountDownLatch asyncCompleted = new CountDownLatch(1);
 
@@ -132,14 +159,15 @@ public class AlbumsRequestTest {
 
     Futures.addCallback(albumFuture, new FutureCallback<List<Album>>() {
       @Override
-      public void onSuccess(List<Album> album) {
-        fail();
+      public void onSuccess(List<Album> albums) {
+        assertEquals(1, albums.size());
+        assertNull(albums.get(0));
+        asyncCompleted.countDown();
       }
 
       @Override
       public void onFailure(Throwable throwable) {
-        TestCase.assertEquals(throwable.getClass(), NotFoundException.class);
-        asyncCompleted.countDown();
+        fail();
       }
 
     });
