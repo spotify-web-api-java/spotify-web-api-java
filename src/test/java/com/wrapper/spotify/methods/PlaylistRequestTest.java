22abf451b8f45e90e4cf745a2a13533d34c4a7d4
Clemens <clemens@heypanda.de>
2015-07-30 08:33:42 +0200

    fixed null-albumType in case of local-tracks (https://github.com/thelinmichael/spotify-web-api-java/issues/37)

--- src/test/java/com/wrapper/spotify/methods/PlaylistRequestTest.java
+++ src/test/java/com/wrapper/spotify/methods/PlaylistRequestTest.java
@@ -3,16 +3,22 @@ package com.wrapper.spotify.methods;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.SettableFuture;
+
 import org.junit.Ignore;
 import org.junit.Test;
+
 import com.wrapper.spotify.Api;
 import com.wrapper.spotify.TestUtil;
+import com.wrapper.spotify.models.Album;
 import com.wrapper.spotify.models.Playlist;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
+import static junit.framework.Assert.assertFalse;
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertTrue;
 import static junit.framework.TestCase.fail;
 
 /*
@@ -62,6 +68,21 @@ public class PlaylistRequestTest {
     assertEquals("https://api.spotify.com/v1/users/thelinmichael/playlists/3ktAYNcRHpazJ9qecm3ptn", playlist.getHref());
   }
 
+  @Test
+  public void shouldBeAbleToHandlePlaylistsWithLocalFiles() throws Exception {
+    final Api api = Api.DEFAULT_API;
+
+    final PlaylistRequest request = api.getPlaylist("thelinmichael", "3ktAYNcRHpazJ9qecm3ptn")
+        .httpManager(TestUtil.MockedHttpManager.returningJson("playlist-localfiles-response.json"))
+        .build();
+
+    Playlist playlist = request.get();
+    
+    assertTrue(playlist.getTracks().getItems().get(0).getTrack().getAlbum().getAlbumType() == null);
+
+    assertNotNull(playlist);
+  }
+  
   @Ignore
   @Test
   public void shouldFailFutureIfPlaylistIsNotFound() throws Exception {
