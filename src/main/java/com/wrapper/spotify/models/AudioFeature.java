f2d3758e55c3497c171de45bfc300df3bc42a8da
bjozzi <bjozzi@gmail.com>
2016-04-17 14:19:03 +0200

    added ability to get audio features

---
+++ src/main/java/com/wrapper/spotify/models/AudioFeature.java
@@ -0,0 +1,167 @@
+package com.wrapper.spotify.models;
+
+
+public class AudioFeature {
+    private double danceability;
+    private double energy;
+    private int key;
+    private double loudness;
+    private int mode;
+    private double speechiness;
+    private double acousticness;
+    private double instrumentalness;
+    private double liveness;
+    private double valence;
+    private double tempo;
+    private String type;
+    private String id;
+    private String uri;
+    private String trackHref;
+    private String analysisUrl;
+    private int durationMs;
+    private int timeSignature;
+
+    public double getDanceability() {
+        return danceability;
+    }
+
+    public void setDanceability(double danceability) {
+        this.danceability = danceability;
+    }
+
+    public double getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(double energy) {
+        this.energy = energy;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public double getLoudness() {
+        return loudness;
+    }
+
+    public void setLoudness(double loudness) {
+        this.loudness = loudness;
+    }
+
+    public int getMode() {
+        return mode;
+    }
+
+    public void setMode(int mode) {
+        this.mode = mode;
+    }
+
+    public double getSpeechiness() {
+        return speechiness;
+    }
+
+    public void setSpeechiness(double speechiness) {
+        this.speechiness = speechiness;
+    }
+
+    public double getAcousticness() {
+        return acousticness;
+    }
+
+    public void setAcousticness(double acousticness) {
+        this.acousticness = acousticness;
+    }
+
+    public double getInstrumentalness() {
+        return instrumentalness;
+    }
+
+    public void setInstrumentalness(double instrumentalness) {
+        this.instrumentalness = instrumentalness;
+    }
+
+    public double getLiveness() {
+        return liveness;
+    }
+
+    public void setLiveness(double liveness) {
+        this.liveness = liveness;
+    }
+
+    public double getValence() {
+        return valence;
+    }
+
+    public void setValence(double valence) {
+        this.valence = valence;
+    }
+
+    public double getTempo() {
+        return tempo;
+    }
+
+    public void setTempo(double tempo) {
+        this.tempo = tempo;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+    public String getTrackHref() {
+        return trackHref;
+    }
+
+    public void setTrackHref(String trackHref) {
+        this.trackHref = trackHref;
+    }
+
+    public String getAnalysisUrl() {
+        return analysisUrl;
+    }
+
+    public void setAnalysisUrl(String analysisUrl) {
+        this.analysisUrl = analysisUrl;
+    }
+
+    public int getDurationMs() {
+        return durationMs;
+    }
+
+    public void setDurationMs(int durationMs) {
+        this.durationMs = durationMs;
+    }
+
+    public int getTimeSignature() {
+        return timeSignature;
+    }
+
+    public void setTimeSignature(int timeSignature) {
+        this.timeSignature = timeSignature;
+    }
+}
