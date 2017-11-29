057b14452b5702923e966ac659cfd8b69986e0db
Jonas Thelemann <e-mail@jonas-thelemann.de>
2017-11-29 14:01:18 +0100

    Builder: Cursor
    ~ Cursor object rewrite for builder implementation

--- src/main/java/com/wrapper/spotify/objects/Cursor.java
+++ src/main/java/com/wrapper/spotify/objects/Cursor.java
@@ -1,14 +1,34 @@
 package com.wrapper.spotify.objects;
 
-public class Cursor {
+public class Cursor extends AbstractModelObject {
+  private final String after;
 
-  private String after;
+  private Cursor(final Cursor.Builder builder) {
+    super(builder);
+
+    this.after = builder.after;
+  }
 
   public String getAfter() {
     return after;
   }
 
-  public void setAfter(String after) {
-    this.after = after;
+  @Override
+  public Builder builder() {
+    return new Builder();
+  }
+
+  public static final class Builder extends AbstractModelObject.Builder<Cursor.Builder> {
+    private String after;
+
+    public Builder setAfter(String after) {
+      this.after = after;
+      return this;
+    }
+
+    @Override
+    public Cursor build() {
+      return new Cursor(this);
+    }
   }
 }
