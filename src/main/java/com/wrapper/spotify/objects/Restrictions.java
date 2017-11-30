7a5b52861841b86a0177b366bf246845846f5d5d
Jonas Thelemann <e-mail@jonas-thelemann.de>
2017-11-30 02:48:03 +0100

    Builder: Restrictions
    ~ Restrictions object rewrite for builder implementation

--- src/main/java/com/wrapper/spotify/objects/Restrictions.java
+++ src/main/java/com/wrapper/spotify/objects/Restrictions.java
@@ -1,14 +1,34 @@
 package com.wrapper.spotify.objects;
 
-public class Restrictions {
+public class Restrictions extends AbstractModelObject {
+  private final String reason;
 
-  private String reason;
+  private Restrictions(final Restrictions.Builder builder) {
+    super(builder);
+
+    this.reason = builder.reason;
+  }
 
   public String getReason() {
     return reason;
   }
 
-  public void setReason(String reason) {
-    this.reason = reason;
+  @Override
+  public Builder builder() {
+    return new Builder();
+  }
+
+  public static final class Builder extends AbstractModelObject.Builder<Restrictions.Builder> {
+    private String reason;
+
+    public Builder setReason(String reason) {
+      this.reason = reason;
+      return this;
+    }
+
+    @Override
+    public Restrictions build() {
+      return new Restrictions(this);
+    }
   }
 }
