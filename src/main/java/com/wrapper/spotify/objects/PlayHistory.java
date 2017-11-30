36c70aab135b5eab850663415ffced90e1b2cd2d
Jonas Thelemann <e-mail@jonas-thelemann.de>
2017-11-30 02:46:58 +0100

    Builder: PlayHistory
    ~ PlayHistory object rewrite for builder implementation

--- src/main/java/com/wrapper/spotify/objects/PlayHistory.java
+++ src/main/java/com/wrapper/spotify/objects/PlayHistory.java
@@ -2,33 +2,59 @@ package com.wrapper.spotify.objects;
 
 import java.util.Date;
 
-public class PlayHistory {
+public class PlayHistory extends AbstractModelObject {
+  private final TrackSimplified track;
+  private final Date playedAt;
+  private final Context context;
 
-  private TrackSimplified track;
-  private Date playedAt;
-  private Context context;
+  private PlayHistory(final PlayHistory.Builder builder) {
+    super(builder);
+
+    this.track = builder.track;
+    this.playedAt = builder.playedAt;
+    this.context = builder.context;
+  }
 
   public TrackSimplified getTrack() {
     return track;
-  }
-
-  public void setTrack(TrackSimplified track) {
-    this.track = track;
   }
 
   public Date getPlayedAt() {
     return playedAt;
   }
 
-  public void setPlayedAt(Date playedAt) {
-    this.playedAt = playedAt;
-  }
-
   public Context getContext() {
     return context;
   }
 
-  public void setContext(Context context) {
-    this.context = context;
+  @Override
+  public Builder builder() {
+    return new Builder();
+  }
+
+  public static final class Builder extends AbstractModelObject.Builder<PlayHistory.Builder> {
+    private TrackSimplified track;
+    private Date playedAt;
+    private Context context;
+
+    public Builder setTrack(TrackSimplified track) {
+      this.track = track;
+      return this;
+    }
+
+    public Builder setPlayedAt(Date playedAt) {
+      this.playedAt = playedAt;
+      return this;
+    }
+
+    public Builder setContext(Context context) {
+      this.context = context;
+      return this;
+    }
+
+    @Override
+    public PlayHistory build() {
+      return new PlayHistory(this);
+    }
   }
 }
