554a72ad6994a0e7f298fa474fead96412583b65
Jonas Thelemann <e-mail@jonas-thelemann.de>
2017-11-30 02:46:09 +0100

    Builder: Error
    ~ Error object rewrite for builder implementation

--- src/main/java/com/wrapper/spotify/objects/Error.java
+++ src/main/java/com/wrapper/spotify/objects/Error.java
@@ -1,23 +1,46 @@
 package com.wrapper.spotify.objects;
 
-public class Error {
+public class Error extends AbstractModelObject {
+  private final int status;
+  private final String message;
 
-  private int status;
-  private String message;
+  private Error(final Error.Builder builder) {
+    super(builder);
+
+    this.status = builder.status;
+    this.message = builder.message;
+  }
 
   public int getStatus() {
     return status;
-  }
-
-  public void setStatus(int status) {
-    this.status = status;
   }
 
   public String getMessage() {
     return message;
   }
 
-  public void setMessage(String message) {
-    this.message = message;
+  @Override
+  public Builder builder() {
+    return new Builder();
+  }
+
+  public static final class Builder extends AbstractModelObject.Builder<Error.Builder> {
+    private int status;
+    private String message;
+
+    public Builder setStatus(int status) {
+      this.status = status;
+      return this;
+    }
+
+    public Builder setMessage(String message) {
+      this.message = message;
+      return this;
+    }
+
+    @Override
+    public Error build() {
+      return new Error(this);
+    }
   }
 }
