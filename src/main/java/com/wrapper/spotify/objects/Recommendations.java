53ecd38e2104da682904c9936c2009fcfc359f6f
Jonas Thelemann <e-mail@jonas-thelemann.de>
2017-11-30 02:47:33 +0100

    Builde: Recommendations
    ~ Recommendations object rewrite for builder implementation

--- src/main/java/com/wrapper/spotify/objects/Recommendations.java
+++ src/main/java/com/wrapper/spotify/objects/Recommendations.java
@@ -2,24 +2,47 @@ package com.wrapper.spotify.objects;
 
 import java.util.List;
 
-public class Recommendations {
+public class Recommendations extends AbstractModelObject {
+  private final RecommendationsSeed seeds;
+  private final List<TrackSimplified> tracks;
 
-  private RecommendationsSeed seeds;
-  private List<TrackSimplified> tracks;
+  private Recommendations(final Recommendations.Builder builder) {
+    super(builder);
+
+    this.seeds = builder.seeds;
+    this.tracks = builder.tracks;
+  }
 
   public RecommendationsSeed getSeeds() {
     return seeds;
-  }
-
-  public void setSeeds(RecommendationsSeed seeds) {
-    this.seeds = seeds;
   }
 
   public List<TrackSimplified> getTracks() {
     return tracks;
   }
 
-  public void setTracks(List<TrackSimplified> tracks) {
-    this.tracks = tracks;
+  @Override
+  public Builder builder() {
+    return new Builder();
+  }
+
+  public static final class Builder extends AbstractModelObject.Builder<Recommendations.Builder> {
+    private RecommendationsSeed seeds;
+    private List<TrackSimplified> tracks;
+
+    public Builder setSeeds(RecommendationsSeed seeds) {
+      this.seeds = seeds;
+      return this;
+    }
+
+    public Builder setTracks(List<TrackSimplified> tracks) {
+      this.tracks = tracks;
+      return this;
+    }
+
+    @Override
+    public Recommendations build() {
+      return new Recommendations(this);
+    }
   }
 }
