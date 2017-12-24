6a5ac3fb15ac85d83fd61291de26a8bb29f7d39a
Christian Schuhmann <christian@schuhmann.pro>
2017-12-24 09:42:35 +0100

    Added "generic" Javadoc comments Comments belonging to interface methods are "copied" to all implementations of its method. This saves some time.

--- src/main/java/com/wrapper/spotify/model_objects/IModelObject.java
+++ src/main/java/com/wrapper/spotify/model_objects/IModelObject.java
@@ -8,16 +8,48 @@ import com.wrapper.spotify.model_objects.specification.PagingCursorbased;
 
 import java.text.SimpleDateFormat;
 
+/**
+ * Interface with methods used in model objects.
+ */
 public interface IModelObject {
   SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'hh:mm:ss");
 
+  /**
+   * Create a builder for building an instance of a model object.<br>
+   * The type of the builder and its methods depend on its corresponding implementation.
+   *
+   * @return A builder object.
+   */
   Builder builder();
 
+  /**
+   * Interface with methods used in builder classes of model objects.
+   */
   interface Builder {
+
+    /**
+     * Build a model object with the information set in the builder object. <br>
+     * The type of the model object and its methods depend on its corresponding implementation.
+     *
+     * @return A model object.
+     */
     IModelObject build();
   }
 
+  /**
+   * Interface with methods used in JsonUtil classes of model objects.
+   *
+   * @param <T> Type of the corresponding model object.
+   */
   interface IJsonUtil<T> {
+
+    /**
+     * Build a model object with the information given in a json object. <br>
+     * The type of the model object and its methods depend on its corresponding implementation.
+     *
+     * @param jsonObject A json object.
+     * @return A model object. The type depends on this methods implementation.
+     */
     T createModelObject(JsonObject jsonObject);
 
     T[] createModelObjectArray(JsonArray jsonArray);
