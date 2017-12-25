7a13b65d82ff06882741e6f8090d618db1506a82
Jonas Thelemann <e-mail@jonas-thelemann.de>
2017-12-25 22:28:29 +0100

    Authentication Error Object
    + "AuthenticationError" Object

---
+++ src/main/java/com/wrapper/spotify/model_objects/AuthenticationError.java
@@ -0,0 +1,61 @@
+package com.wrapper.spotify.model_objects;
+
+import com.google.gson.JsonObject;
+
+public class AuthenticationError extends AbstractModelObject {
+  private final String error;
+  private final String error_description;
+
+  private AuthenticationError(final AuthenticationError.Builder builder) {
+    super(builder);
+
+    this.error = builder.error;
+    this.error_description = builder.error_description;
+  }
+
+  public String getError() {
+    return error;
+  }
+
+  public String getError_description() {
+    return error_description;
+  }
+
+  @Override
+  public Builder builder() {
+    return new Builder();
+  }
+
+  public static final class Builder extends AbstractModelObject.Builder {
+    private String error;
+    private String error_description;
+
+    public Builder setError(String error) {
+      this.error = error;
+      return this;
+    }
+
+    public Builder setError_description(String error_description) {
+      this.error_description = error_description;
+      return this;
+    }
+
+    @Override
+    public AuthenticationError build() {
+      return new AuthenticationError(this);
+    }
+  }
+
+  public static final class JsonUtil extends AbstractModelObject.JsonUtil<AuthenticationError> {
+    public AuthenticationError createModelObject(JsonObject jsonObject) {
+      if (jsonObject == null || jsonObject.isJsonNull()) {
+        return null;
+      }
+
+      return new AuthenticationError.Builder()
+              .setError(jsonObject.get("error").getAsString())
+              .setError_description(jsonObject.get("error_description").getAsString())
+              .build();
+    }
+  }
+}
