b0d4f36602c74af14cfd470b0c173868b8c86e64
Jonas Thelemann <e-mail@jonas-thelemann.de>
2017-12-30 00:45:45 +0100

    IModelObject Methods
    + Method list for IJsonUtil

--- src/main/java/com/wrapper/spotify/model_objects/IModelObject.java
+++ src/main/java/com/wrapper/spotify/model_objects/IModelObject.java
@@ -1,5 +1,7 @@
 package com.wrapper.spotify.model_objects;
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 import java.text.SimpleDateFormat;
@@ -15,6 +17,26 @@ public interface IModelObject {
 
   interface IJsonUtil<T> {
     T createModelObject(JsonObject jsonObject);
+
+    T[] createModelObjectArray(JsonArray jsonArray);
+
+    T[] createModelObjectArray(String json);
+
+    T[] createModelObjectArray(String json, String key);
+
+    <X> X[] createModelObjectArray(JsonArray jsonArray, TypeToken<X> typeToken);
+
+    Paging<T> createModelObjectPaging(JsonObject jsonObject);
+
+    Paging<T> createModelObjectPaging(String json);
+
+    Paging<T> createModelObjectPaging(String json, String key);
+
+    PagingCursorbased<T> createModelObjectPagingCursorbased(JsonObject jsonObject);
+
+    PagingCursorbased<T> createModelObjectPagingCursorbased(String json);
+
+    PagingCursorbased<T> createModelObjectPagingCursorbased(String json, String key);
   }
 
 //  interface IJsonUtilPaging {
