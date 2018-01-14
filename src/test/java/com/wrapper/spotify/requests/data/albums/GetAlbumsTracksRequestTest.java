67db5b495ec0936c77d29501d952c5a31a92a135
Jonas Thelemann <e-mail@jonas-thelemann.de>
2018-01-14 06:33:19 +0100

    Model Object Tests
    - "model_object" tests

--- src/test/java/com/wrapper/spotify/requests/data/albums/GetAlbumsTracksRequestTest.java
+++ src/test/java/com/wrapper/spotify/requests/data/albums/GetAlbumsTracksRequestTest.java
@@ -13,7 +13,6 @@ import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 
 @RunWith(MockitoJUnitRunner.class)
 public class GetAlbumsTracksRequestTest implements ITest<Paging<TrackSimplified>> {
