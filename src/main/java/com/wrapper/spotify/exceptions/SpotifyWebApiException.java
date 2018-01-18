ef742bd3d88da951bbe2a87f09003aa4475b6cdb
Christian Schuhmann <christian@schuhmann.pro>
2018-01-18 10:53:23 +0100

    Javadoc Exceptions + SpotifyWebApiException The other exception classes already have some Javadoc.

--- src/main/java/com/wrapper/spotify/exceptions/SpotifyWebApiException.java
+++ src/main/java/com/wrapper/spotify/exceptions/SpotifyWebApiException.java
@@ -2,6 +2,9 @@ package com.wrapper.spotify.exceptions;
 
 import org.apache.http.HttpException;
 
+/**
+ * An exception happened, eg. a HTTP status code 4** or 5** has been returned in a request.
+ */
 public class SpotifyWebApiException extends HttpException {
 
   public SpotifyWebApiException() {
