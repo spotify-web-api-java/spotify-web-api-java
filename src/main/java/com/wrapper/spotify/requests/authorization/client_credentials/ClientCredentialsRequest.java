3a3362fbbf9c74fdc8e18ecc39e0f9595c3bc965
Christian Schuhmann <christian8246q@gmail.com>
2018-01-28 19:04:58 +0100

    Javadoc Auth Requests

--- src/main/java/com/wrapper/spotify/requests/authorization/client_credentials/ClientCredentialsRequest.java
+++ src/main/java/com/wrapper/spotify/requests/authorization/client_credentials/ClientCredentialsRequest.java
@@ -7,12 +7,24 @@ import com.wrapper.spotify.requests.authorization.AbstractAthorizationRequest;
 
 import java.io.IOException;
 
+/**
+ * Request an access token by creating a
+ * <a href="https://developer.spotify.com/web-api/authorization-guide/#client-credentials-flow">Client Credentials</a>
+ * request.
+ */
 public class ClientCredentialsRequest extends AbstractAthorizationRequest {
 
   public ClientCredentialsRequest(Builder builder) {
     super(builder);
   }
 
+  /**
+   * Request an access token.
+   *
+   * @return A {@link ClientCredentials} object containing an access token.
+   * @throws IOException            In case of networking issues.
+   * @throws SpotifyWebApiException The Web API returned an error further specified in this exception's root cause.
+   */
   @SuppressWarnings("unchecked")
   public ClientCredentials execute() throws
           IOException,
@@ -20,18 +32,32 @@ public class ClientCredentialsRequest extends AbstractAthorizationRequest {
     return new ClientCredentials.JsonUtil().createModelObject(postJson());
   }
 
+  /**
+   * Builder class for building a {@link ClientCredentialsRequest}.
+   */
   public static final class Builder extends AbstractAthorizationRequest.Builder<Builder> {
 
     public Builder(final String clientId, final String clientSecret) {
       super(clientId, clientSecret);
     }
 
+    /**
+     * The grant type setter.
+     *
+     * @param grant_type Required. Set it to {@code "client_credentials"}.
+     * @return A {@link ClientCredentialsRequest.Builder}.
+     */
     public Builder grant_type(final String grant_type) {
       assert (grant_type != null);
       assert (grant_type.equals("client_credentials"));
       return setFormParameter("grant_type", grant_type);
     }
 
+    /**
+     * The request build method.
+     *
+     * @return A {@link ClientCredentialsRequest}.
+     */
     public ClientCredentialsRequest build() {
       setHeader("Content-Type", "application/x-www-form-urlencoded");
       setHost(SpotifyApi.DEFAULT_AUTHENTICATION_HOST);
