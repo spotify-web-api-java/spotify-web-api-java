48fe858f0030a6a55a8b294a5e8e5de966ac1dc7
Christian Schuhmann <christian8246q@gmail.com>
2018-01-28 19:26:25 +0100

    Javadoc Album Requests

--- src/main/java/com/wrapper/spotify/requests/data/albums/GetAlbumsTracksRequest.java
+++ src/main/java/com/wrapper/spotify/requests/data/albums/GetAlbumsTracksRequest.java
@@ -14,10 +14,22 @@ import java.io.IOException;
  */
 public class GetAlbumsTracksRequest extends AbstractDataRequest {
 
+  /**
+   * The private {@link GetAlbumsTracksRequest} constructor.
+   *
+   * @param builder A {@link GetAlbumsTracksRequest.Builder}.
+   */
   private GetAlbumsTracksRequest(final Builder builder) {
     super(builder);
   }
 
+  /**
+   * Get the tracks from the album.
+   *
+   * @return A track paging.
+   * @throws IOException            In case of networking issues.
+   * @throws SpotifyWebApiException The Web API returned an error further specified in this exception's root cause.
+   */
   @SuppressWarnings("unchecked")
   public Paging<TrackSimplified> execute() throws
           IOException,
@@ -25,6 +37,9 @@ public class GetAlbumsTracksRequest extends AbstractDataRequest {
     return new TrackSimplified.JsonUtil().createModelObjectPaging(getJson());
   }
 
+  /**
+   * Builder class for building a {@link GetAlbumsTracksRequest}.
+   */
   public static final class Builder extends AbstractDataRequest.Builder<Builder> {
 
     public Builder(final String accessToken) {
@@ -32,10 +47,11 @@ public class GetAlbumsTracksRequest extends AbstractDataRequest {
     }
 
     /**
-     * The id path parameter setter.
+     * The ID path parameter setter.
      *
      * @param id The Spotify ID for the album.
-     * @return A GetAlbumsTracksRequest builder.
+     * @return A {@link GetAlbumsTracksRequest.Builder}.
+     * @see <a href="https://developer.spotify.com/web-api/user-guide/#spotify-uris-and-ids">Spotify URIs &amp; IDs</a>
      */
     public Builder id(final String id) {
       assert (id != null);
@@ -47,7 +63,7 @@ public class GetAlbumsTracksRequest extends AbstractDataRequest {
      * The limit query parameter setter.
      *
      * @param limit Optional. The maximum number of tracks to return. Default: 20. Minimum: 1. Maximum: 50.
-     * @return A GetAlbumsTracksRequest builder.
+     * @return A {@link GetAlbumsTracksRequest.Builder}.
      */
     public Builder limit(final Integer limit) {
       assert (1 <= limit && limit <= 50);
@@ -59,7 +75,7 @@ public class GetAlbumsTracksRequest extends AbstractDataRequest {
      *
      * @param offset Optional. The index of the first track to return. Default: 0 (the first object). Use with limit to
      *               get the next set of tracks.
-     * @return A GetAlbumsTracksRequest builder.
+     * @return A {@link GetAlbumsTracksRequest.Builder}.
      */
     public Builder offset(final Integer offset) {
       assert (offset >= 0);
@@ -71,13 +87,20 @@ public class GetAlbumsTracksRequest extends AbstractDataRequest {
      *
      * @param market Optional. An ISO 3166-1 alpha-2 country code. Provide this parameter if you want to apply Track
      *               Relinking.
-     * @return A GetAlbumsTracksRequest builder.
+     * @return A {@link GetAlbumsTracksRequest.Builder}.
+     * @see <a href="https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2">Wikipedia: ISO 3166-1 alpha-2 country codes</a>
+     * @see <a href="https://developer.spotify.com/web-api/track-relinking-guide/">Spotify: Track Relinking Guide</a>
      */
     public Builder market(final CountryCode market) {
       assert (market != null);
       return setQueryParameter("market", market);
     }
 
+    /**
+     * The request build method.
+     *
+     * @return A custom {@link GetAlbumsTracksRequest}.
+     */
     @Override
     public GetAlbumsTracksRequest build() {
       setPath("/v1/albums/{id}/tracks");
