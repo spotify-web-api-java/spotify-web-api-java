edd2a351db1bcd52ef3ff4518b1ae13ffbaf5c9c
Christian Schuhmann <christian8246q@gmail.com>
2018-02-03 13:37:51 +0100

    Javadoc Playlist Requests

--- src/main/java/com/wrapper/spotify/requests/data/playlists/RemoveTracksFromPlaylistRequest.java
+++ src/main/java/com/wrapper/spotify/requests/data/playlists/RemoveTracksFromPlaylistRequest.java
@@ -7,12 +7,29 @@ import com.wrapper.spotify.requests.data.AbstractDataRequest;
 
 import java.io.IOException;
 
+/**
+ * Remove one or more tracks from a user’s playlist.
+ */
 public class RemoveTracksFromPlaylistRequest extends AbstractDataRequest {
 
+  /**
+   * The private {@link RemoveTracksFromPlaylistRequest} constructor.
+   *
+   * @param builder A {@link RemoveTracksFromPlaylistRequest.Builder}.
+   */
   private RemoveTracksFromPlaylistRequest(final Builder builder) {
     super(builder);
   }
 
+  /**
+   * Remove tracks from a playlist.
+   *
+   * @return A playlist snapshot ID. The snapshot ID can be used to identify your playlist version in future requests.
+   * @see <a href="https://developer.spotify.com/web-api/working-with-playlists/#version-control-and-snapshots">
+   *      Spotify: Version Control and Snapshots</a>
+   * @throws IOException            In case of networking issues.
+   * @throws SpotifyWebApiException The Web API returned an error further specified in this exception's root cause.
+   */
   @SuppressWarnings("unchecked")
   public SnapshotResult execute() throws
           IOException,
@@ -20,36 +37,98 @@ public class RemoveTracksFromPlaylistRequest extends AbstractDataRequest {
     return new SnapshotResult.JsonUtil().createModelObject(getJson());
   }
 
+  /**
+   * Builder class for building a {@link RemoveTracksFromPlaylistRequest}.
+   */
   public static final class Builder extends AbstractDataRequest.Builder<Builder> {
 
+    /**
+     * Create a new {@link RemoveTracksFromPlaylistRequest.Builder}.
+     * <p>
+     * Removing tracks from an user's public playlists requires authorization of the {@code playlist-modify-public}
+     * scope; removing tracks from an user's private playlist (including collaborative playlists) requires the
+     * {@code playlist-modify-private} scope.
+     *
+     * @param accessToken Required. A valid access token from the Spotify Accounts service.
+     * @see <a href="https://developer.spotify.com/web-api/using-scopes/">Spotify: Using Scopes</a>
+     */
     public Builder(final String accessToken) {
       super(accessToken);
     }
 
+    /**
+     * The user ID setter.
+     *
+     * @param user_id The user's Spotify user ID.
+     * @return An {@link RemoveTracksFromPlaylistRequest.Builder}.
+     * @see <a href="https://developer.spotify.com/web-api/user-guide/#spotify-uris-and-ids">Spotify: URIs &amp; IDs</a>
+     */
     public Builder user_id(final String user_id) {
       assert (user_id != null);
       assert (!user_id.equals(""));
       return setPathParameter("user_id", user_id);
     }
 
+    /**
+     * The playlist ID setter.
+     *
+     * @param playlist_id The Spotify ID for the playlist.
+     * @return An {@link RemoveTracksFromPlaylistRequest.Builder}.
+     * @see <a href="https://developer.spotify.com/web-api/user-guide/#spotify-uris-and-ids">Spotify: URIs &amp; IDs</a>
+     */
     public Builder playlist_id(final String playlist_id) {
       assert (playlist_id != null);
       assert (!playlist_id.equals(""));
       return setPathParameter("playlist_id", playlist_id);
     }
 
+    /**
+     * The track URIs setter.
+     * <p>
+     * There are several ways to specify which tracks to remove, determined by the request parameters.
+     * Removing all occurrences of specific tracks: <br>
+     * {@code { "tracks": [{ "uri": "spotify:track:4iV5W9uYEdYUVa79Axb7Rh" },
+     * {"uri": "spotify:track:1301WleyT98MSxVHPZCA6M" }] }} <br>
+     * Removing a specific occurrence of a track: <br>
+     * {@code { "tracks": [{ "uri": "spotify:track:4iV5W9uYEdYUVa79Axb7Rh", "positions": [0,3] },
+     * { "uri": "spotify:track:1301WleyT98MSxVHPZCA6M", "positions": [7] }] }}
+     *
+     * @param tracks Required. An array of objects containing Spotify URIs of the tracks to remove.
+     * @return A {@link RemoveTracksFromPlaylistRequest.Builder}.
+     * @see <a href="https://developer.spotify.com/web-api/user-guide/#spotify-uris-and-ids">Spotify: URIs &amp; IDs</a>
+     */
     public Builder tracks(final JsonArray tracks) {
       assert (tracks != null);
       assert (!tracks.isJsonNull());
       return setBodyParameter("tracks", tracks);
     }
 
+    /**
+     * The playlist snapshot ID setter.
+     * <p>
+     * To guard against errors when concurrent edits happen to the same playlist, we recommend specifying a snapshot ID.
+     * The snapshot ID lets us know which version of the playlist you are trying to edit. Concurrent edits by another
+     * user will be automatically resolved. If a given track in a given position is not found in the specified snapshot,
+     * the entire request will fail an no edits will take place.
+     *
+     * @param snapshotId Optional. The playlist's snapshot ID against which you want to make the changes. The API will
+     *                   validate that the specified tracks exist and in the specified positions and make the changes,
+     *                   even if more recent changes have been made to the playlist.
+     * @return A {@link RemoveTracksFromPlaylistRequest.Builder}.
+     * @see <a href="https://developer.spotify.com/web-api/working-with-playlists/#version-control-and-snapshots">
+     *      Spotify: Version Control and Snapshots</a>
+     */
     public Builder snapshotId(final String snapshotId) {
       assert (snapshotId != null);
       assert (!snapshotId.equals(""));
       return setBodyParameter("snapshot_id", snapshotId);
     }
 
+    /**
+     * The request build method.
+     *
+     * @return A custom {@link RemoveTracksFromPlaylistRequest}.
+     */
     @Override
     public RemoveTracksFromPlaylistRequest build() {
       setHeader("Content-Type", "application/json");
--- src/main/java/com/wrapper/spotify/requests/data/playlists/ReorderPlaylistsTracksRequest.java
+++ src/main/java/com/wrapper/spotify/requests/data/playlists/ReorderPlaylistsTracksRequest.java
@@ -6,12 +6,33 @@ import com.wrapper.spotify.requests.data.AbstractDataRequest;
 
 import java.io.IOException;
 
+/**
+ * Reorder a track or a group of tracks in a playlist.
+ * <p>
+ * When reordering tracks, the timestamp indicating when they were added and the user who added them will
+ * be kept untouched. In addition, the users following the playlists won’t be notified about changes in
+ * the playlists when the tracks are reordered.
+ */
 public class ReorderPlaylistsTracksRequest extends AbstractDataRequest {
 
+  /**
+   * The private {@link ReorderPlaylistsTracksRequest} constructor.
+   *
+   * @param builder A {@link ReorderPlaylistsTracksRequest.Builder}.
+   */
   private ReorderPlaylistsTracksRequest(final Builder builder) {
     super(builder);
   }
 
+  /**
+   * Reorder the tracks in a playlist.
+   *
+   * @return A playlist snapshot ID. The snapshot ID can be used to identify your playlist version in future requests.
+   * @see <a href="https://developer.spotify.com/web-api/working-with-playlists/#version-control-and-snapshots">
+   *      Spotify: Version Control and Snapshots</a>
+   * @throws IOException            In case of networking issues.
+   * @throws SpotifyWebApiException The Web API returned an error further specified in this exception's root cause.
+   */
   @SuppressWarnings("unchecked")
   public SnapshotResult execute() throws
           IOException,
@@ -19,48 +40,107 @@ public class ReorderPlaylistsTracksRequest extends AbstractDataRequest {
     return new SnapshotResult.JsonUtil().createModelObject(putJson());
   }
 
+  /**
+   * Builder class for building a {@link ReorderPlaylistsTracksRequest}.
+   */
   public static final class Builder extends AbstractDataRequest.Builder<Builder> {
 
+    /**
+     * Create a new {@link ReorderPlaylistsTracksRequest.Builder}.
+     * <p>
+     * Reordering tracks in the current user's public playlists requires authorization of the
+     * {@code playlist-modify-public} scope; reordering tracks in the current user's private playlist (including
+     * collaborative playlists) requires the {@code playlist-modify-private} scope.
+     *
+     * @param accessToken Required. A valid access token from the Spotify Accounts service.
+     * @see <a href="https://developer.spotify.com/web-api/using-scopes/">Spotify: Using Scopes</a>
+     */
     public Builder(final String accessToken) {
       super(accessToken);
     }
 
+    /**
+     * The user ID setter.
+     *
+     * @param user_id The user's Spotify user ID.
+     * @return A {@link ReorderPlaylistsTracksRequest.Builder}.
+     * @see <a href="https://developer.spotify.com/web-api/user-guide/#spotify-uris-and-ids">Spotify: URIs &amp; IDs</a>
+     */
     public Builder user_id(final String user_id) {
       assert (user_id != null);
       assert (!user_id.equals(""));
       return setPathParameter("user_id", user_id);
     }
 
+    /**
+     * The playlist ID setter.
+     *
+     * @param playlist_id The Spotify ID for the playlist.
+     * @return A {@link ReorderPlaylistsTracksRequest.Builder}.
+     * @see <a href="https://developer.spotify.com/web-api/user-guide/#spotify-uris-and-ids">Spotify: URIs &amp; IDs</a>
+     */
     public Builder playlist_id(final String playlist_id) {
       assert (playlist_id != null);
       assert (!playlist_id.equals(""));
       return setPathParameter("playlist_id", playlist_id);
     }
 
+    /**
+     * The range start setter.
+     *
+     * @param range_start Required. The position of the first track to be reordered.
+     * @return A {@link ReorderPlaylistsTracksRequest.Builder}.
+     */
     public Builder range_start(final Integer range_start) {
       assert (range_start != null);
       assert (range_start >= 0);
       return setBodyParameter("range_start", range_start);
     }
 
+    /**
+     * The range length setter.
+     *
+     * @param range_length 	Optional. The amount of tracks to be reordered. Defaults to 1 if not set.
+     * @return A {@link ReorderPlaylistsTracksRequest.Builder}.
+     */
     public Builder range_length(final Integer range_length) {
       assert (range_length != null);
       assert (range_length >= 1);
       return setBodyParameter("range_length", range_length);
     }
 
+    /**
+     * The insert before setter.
+     *
+     * @param insert_before Required. The position where the tracks should be inserted. To reorder the tracks to the
+     *                      end of the playlist, simply set insert_before to the position after the last track.
+     * @return A {@link ReorderPlaylistsTracksRequest.Builder}.
+     */
     public Builder insert_before(final Integer insert_before) {
       assert (insert_before != null);
       assert (insert_before >= 0);
       return setBodyParameter("insert_before", insert_before);
     }
 
+    /**
+     * The playlist snapshot ID setter.
+     *
+     * @param snapshotId Optional. The playlist's snapshot ID against which you want to make the changes.
+     * @return A {@link RemoveTracksFromPlaylistRequest.Builder}.
+     * @see <a href="https://developer.spotify.com/web-api/working-with-playlists/#version-control-and-snapshots">
+     *      Spotify: Version Control and Snapshots</a>
+     */
     public Builder snapshot_id(final String snapshot_id) {
       assert (snapshot_id != null);
       assert (!snapshot_id.equals(""));
       return setBodyParameter("snapshot_id", snapshot_id);
     }
 
+    /**
+     * The request build method.
+     *
+     * @return A custom {@link ReorderPlaylistsTracksRequest}.
+     */
     @Override
     public ReorderPlaylistsTracksRequest build() {
       setPath("/v1/users/{user_id}/playlists/{playlist_id}/tracks");
