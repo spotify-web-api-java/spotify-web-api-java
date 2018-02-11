0c68c570ecde7741c47463948aa039f1bbc70655
Jonas Thelemann <e-mail@jonas-thelemann.de>
2018-02-11 08:08:50 +0100

    1.7.5
    ~ Version update
    ~ Travis strap

--- travis/before-deploy.sh
+++ travis/before-deploy.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 openssl aes-256-cbc -K $encrypted_3e6b49107f67_key -iv $encrypted_3e6b49107f67_iv -in travis/codesigning.asc.enc -out travis/codesigning.asc -d
-echo "allow-loopback-pinentry" > ~/.gnupg/gpg-agent.conf
-echo "pinentry-mode loopback" > ~/.gnupg/gpg.conf
-echo "use-agent" >> ~/.gnupg/gpg.conf
-cat ~/.gnupg/gpg.conf
-gpg --list-secret-keys
 gpg --batch --fast-import travis/codesigning.asc
-gpg --list-secret-keys
