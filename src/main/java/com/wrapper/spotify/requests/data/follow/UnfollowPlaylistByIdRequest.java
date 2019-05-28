2cc04381dd2c553b3050bbfd438aa519625e3387
Jonas Thelemann <e-mail@jonas-thelemann.de>
2019-05-28 21:03:26 +0200

    2 Spaces Indentation

--- src/main/java/com/wrapper/spotify/requests/data/follow/UnfollowPlaylistByIdRequest.java
+++ src/main/java/com/wrapper/spotify/requests/data/follow/UnfollowPlaylistByIdRequest.java
@@ -10,69 +10,69 @@ import java.io.IOException;
  */
 public class UnfollowPlaylistByIdRequest extends AbstractDataRequest {
 
+  /**
+   * The private {@link UnfollowPlaylistByIdRequest} constructor.
+   *
+   * @param builder A {@link UnfollowPlaylistByIdRequest.Builder}.
+   */
+  private UnfollowPlaylistByIdRequest(final Builder builder) {
+    super(builder);
+  }
+
+  /**
+   * Unfollow a playlist.
+   *
+   * @return A string. <b>Note:</b> This endpoint doesn't return something in its response body.
+   * @throws IOException            In case of networking issues.
+   * @throws SpotifyWebApiException The Web API returned an error further specified in this exception's root cause.
+   */
+  @SuppressWarnings("unchecked")
+  public String execute() throws
+          IOException,
+          SpotifyWebApiException {
+    return deleteJson();
+  }
+
+  /**
+   * Builder class for building an {@link UnfollowPlaylistByIdRequest}.
+   */
+  public static final class Builder extends AbstractDataRequest.Builder<Builder> {
+
     /**
-     * The private {@link UnfollowPlaylistByIdRequest} constructor.
+     * Create a new {@link UnfollowPlaylistByIdRequest.Builder} instance.
+     * <p>
+     * Unfollowing a publicly followed playlist for a user requires authorization of the {@code playlist-modify-public}
+     * scope; unfollowing a privately followed playlist requires the {@code playlist-modify-private} scope.
      *
-     * @param builder A {@link UnfollowPlaylistByIdRequest.Builder}.
+     * @param accessToken Required. A valid access token from the Spotify Accounts service.
+     * @see <a href="https://developer.spotify.com/web-api/using-scopes/">Spotify: Using Scopes</a>
      */
-    private UnfollowPlaylistByIdRequest(final Builder builder) {
-        super(builder);
+    public Builder(final String accessToken) {
+      super(accessToken);
     }
 
     /**
-     * Unfollow a playlist.
+     * The playlist ID setter.
      *
-     * @return A string. <b>Note:</b> This endpoint doesn't return something in its response body.
-     * @throws IOException            In case of networking issues.
-     * @throws SpotifyWebApiException The Web API returned an error further specified in this exception's root cause.
+     * @param playlist_id The Spotify ID of the playlist that is to be no longer followed.
+     * @return An {@link UnfollowPlaylistByIdRequest.Builder}.
+     * @see <a href="https://developer.spotify.com/web-api/user-guide/#spotify-uris-and-ids">Spotify: URIs &amp; IDs</a>
      */
-    @SuppressWarnings("unchecked")
-    public String execute() throws
-            IOException,
-            SpotifyWebApiException {
-        return deleteJson();
+    public Builder playlist_id(final String playlist_id) {
+      assert (playlist_id != null);
+      assert (!playlist_id.equals(""));
+      return setPathParameter("playlist_id", playlist_id);
     }
 
     /**
-     * Builder class for building an {@link UnfollowPlaylistByIdRequest}.
+     * The request build method.
+     *
+     * @return A custom {@link UnfollowPlaylistByIdRequest}.
      */
-    public static final class Builder extends AbstractDataRequest.Builder<Builder> {
-
-        /**
-         * Create a new {@link UnfollowPlaylistByIdRequest.Builder} instance.
-         * <p>
-         * Unfollowing a publicly followed playlist for a user requires authorization of the {@code playlist-modify-public}
-         * scope; unfollowing a privately followed playlist requires the {@code playlist-modify-private} scope.
-         *
-         * @param accessToken Required. A valid access token from the Spotify Accounts service.
-         * @see <a href="https://developer.spotify.com/web-api/using-scopes/">Spotify: Using Scopes</a>
-         */
-        public Builder(final String accessToken) {
-            super(accessToken);
-        }
-
-        /**
-         * The playlist ID setter.
-         *
-         * @param playlist_id The Spotify ID of the playlist that is to be no longer followed.
-         * @return An {@link UnfollowPlaylistByIdRequest.Builder}.
-         * @see <a href="https://developer.spotify.com/web-api/user-guide/#spotify-uris-and-ids">Spotify: URIs &amp; IDs</a>
-         */
-        public Builder playlist_id(final String playlist_id) {
-            assert (playlist_id != null);
-            assert (!playlist_id.equals(""));
-            return setPathParameter("playlist_id", playlist_id);
-        }
-
-        /**
-         * The request build method.
-         *
-         * @return A custom {@link UnfollowPlaylistByIdRequest}.
-         */
-        @Override
-        public UnfollowPlaylistByIdRequest build() {
-            setPath("/v1/playlists/{playlist_id}/followers");
-            return new UnfollowPlaylistByIdRequest(this);
-        }
+    @Override
+    public UnfollowPlaylistByIdRequest build() {
+      setPath("/v1/playlists/{playlist_id}/followers");
+      return new UnfollowPlaylistByIdRequest(this);
     }
+  }
 }
--- src/test/java/com/wrapper/spotify/requests/data/follow/UnfollowPlaylistByIdRequestTest.java
+++ src/test/java/com/wrapper/spotify/requests/data/follow/UnfollowPlaylistByIdRequestTest.java
@@ -15,36 +15,36 @@ import static org.junit.Assert.assertNull;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UnfollowPlaylistByIdRequestTest extends AbstractDataTest<String> {
-    private final UnfollowPlaylistByIdRequest defaultRequest = SPOTIFY_API
-            .unfollowPlaylist(ID_PLAYLIST)
-            .setHttpManager(
-                    TestUtil.MockedHttpManager.returningJson(
-                            "requests/data/follow/FollowArtistsOrUsersRequestTest.json"))
-            .build();
+  private final UnfollowPlaylistByIdRequest defaultRequest = SPOTIFY_API
+          .unfollowPlaylist(ID_PLAYLIST)
+          .setHttpManager(
+                  TestUtil.MockedHttpManager.returningJson(
+                          "requests/data/follow/FollowArtistsOrUsersRequestTest.json"))
+          .build();
 
-    public UnfollowPlaylistByIdRequestTest() throws Exception {
-    }
+  public UnfollowPlaylistByIdRequestTest() throws Exception {
+  }
 
-    @Test
-    public void shouldComplyWithReference() {
-        assertHasAuthorizationHeader(defaultRequest);
-        assertEquals(
-                "https://api.spotify.com:443/v1/playlists/3AGOiaoRXMSjswCLtuNqv5/followers",
-                defaultRequest.getUri().toString());
-    }
+  @Test
+  public void shouldComplyWithReference() {
+    assertHasAuthorizationHeader(defaultRequest);
+    assertEquals(
+            "https://api.spotify.com:443/v1/playlists/3AGOiaoRXMSjswCLtuNqv5/followers",
+            defaultRequest.getUri().toString());
+  }
 
-    @Test
-    public void shouldReturnDefault_sync() throws IOException, SpotifyWebApiException {
-        shouldReturnDefault(defaultRequest.execute());
-    }
+  @Test
+  public void shouldReturnDefault_sync() throws IOException, SpotifyWebApiException {
+    shouldReturnDefault(defaultRequest.execute());
+  }
 
-    @Test
-    public void shouldReturnDefault_async() throws ExecutionException, InterruptedException {
-        shouldReturnDefault((String) defaultRequest.executeAsync().get());
-    }
+  @Test
+  public void shouldReturnDefault_async() throws ExecutionException, InterruptedException {
+    shouldReturnDefault((String) defaultRequest.executeAsync().get());
+  }
 
-    public void shouldReturnDefault(final String string) {
-        assertNull(
-                string);
-    }
+  public void shouldReturnDefault(final String string) {
+    assertNull(
+            string);
+  }
 }
