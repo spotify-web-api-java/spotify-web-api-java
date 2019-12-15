ed50e1537c8ff124185ad12bdf81af518ee32c7c
Mihir Joshi <mihir67mj@gmail.com>
2019-12-15 12:34:38 +0530

    Update code_cov.sh

--- travis/code_cov.sh
+++ travis/code_cov.sh
@@ -1,6 +1,5 @@
 set -ev
-if [ '${TRAVIS_JDK_VERSION}' = 'openjdk8' ]
-  then 
-    mvn cobertura:cobertura 
-    bash <(curl -s https://codecov.io/bash) 
+if [ "${TRAVIS_JDK_VERSION}" = "openjdk8" ] then 
+    mvn cobertura:cobertura
+    bash <(curl -s https://codecov.io/bash)
 fi
