441e301d0d89f334e46665c23ce2709ab4d6c15b
Jonas Thelemann <e-mail@jonas-thelemann.de>
2026-09-01 00:20:52 +0200

    docs(examples): add examples for the 26 uncovered endpoints

---
+++ examples/data/episodes/GetSeveralEpisodesExample.java
@@ -0,0 +1,60 @@
+package data.episodes;
+
+import se.michaelthelin.spotify.SpotifyApi;
+import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+import se.michaelthelin.spotify.model_objects.specification.Episode;
+import se.michaelthelin.spotify.requests.data.episodes.GetSeveralEpisodesRequest;
+import org.apache.hc.core5.http.ParseException;
+
+import java.io.IOException;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+
+/**
+ * @deprecated Get Spotify catalog information for several episodes based on their Spotify IDs.
+ */
+@Deprecated
+public class GetSeveralEpisodesExample {
+  private static final String accessToken = "taHZ2SdB-bPA3FsK3D7ZN5npZS47cMy-IEySVEGttOhXmqaVAIo0ESvTCLjLBifhHOHOIuhFUKPW1WMDP7w6dj3MAZdWT8CLI2MkZaXbYLTeoDvXesf2eeiLYPBGdx8tIwQJKgV8XdnzH_DONk";
+  private static final String ids = "046WGwFKUevqKMA4NQfYUv";
+
+  private static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
+    .setAccessToken(accessToken)
+    .build();
+  private static final GetSeveralEpisodesRequest getSeveralEpisodesRequest = spotifyApi.getSeveralEpisodes(ids)
+//          .market(CountryCode.SE)
+    .build();
+
+  public static void getSeveralEpisodes_Sync() {
+    try {
+      final Episode[] episodes = getSeveralEpisodesRequest.execute();
+
+      System.out.println("Number of episodes: " + episodes.length);
+    } catch (IOException | SpotifyWebApiException | ParseException e) {
+      System.out.println("Error: " + e.getMessage());
+    }
+  }
+
+  public static void getSeveralEpisodes_Async() {
+    try {
+      final CompletableFuture<Episode[]> episodesFuture = getSeveralEpisodesRequest.executeAsync();
+
+      // Thread free to do other tasks...
+
+      // Example Only. Never block in production code.
+      final Episode[] episodes = episodesFuture.join();
+
+      System.out.println("Number of episodes: " + episodes.length);
+    } catch (CompletionException e) {
+      System.out.println("Error: " + e.getCause().getMessage());
+    } catch (CancellationException e) {
+      System.out.println("Async operation cancelled.");
+    }
+  }
+
+  public static void main(String[] args) {
+    getSeveralEpisodes_Sync();
+    getSeveralEpisodes_Async();
+  }
+}
